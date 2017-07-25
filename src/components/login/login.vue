2257f983ae263b921eef79a49156fc96c0df7cd1
jessietang <18380448313@163.com>
2017-07-25 10:37:30 +0800

    登录密码加密处理

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -33,28 +33,245 @@
           userInfo: {
             userId: 0, // 用户id
             zoneId: 0 // 区域id
-          }
+          },
+          //密码
+          hexcase: 1,
+          b64pad: '',
+          chrsz: 8,
+          mode: 32
         }
       },
+    /*
+    * var hexcase = 1;
+     var b64pad = "";
+     var chrsz = 8;
+     var mode = 32;
+    * */
       created () {
-        this.getLocalUsername();
+        this.getLocalUsernameAndPass();
       },
       methods: {
+        // 密码加密
+        md5 (A) {
+          return this.hex_md5(A)
+        },
+        hex_md5(A) {
+          var _this = this;
+          return _this.binl2hex(_this.core_md5(_this.str2binl(A), A.length * _this.chrsz))
+        },
+        str_md5(A) {
+          var _this = this;
+          return _this.binl2str(_this.core_md5(_this.str2binl(A), A.length * _this.chrsz))
+        },
+        hex_hmac_md5(A, B) {
+          var _this = this;
+          return _this.binl2hex(_this.core_hmac_md5(A, B))
+        },
+        b64_hmac_md5(A, B) {
+          var _this = this;
+          return _this.binl2b64(_this.core_hmac_md5(A, B))
+        },
+        str_hmac_md5(A, B) {
+          var _this = this;
+          return _this.binl2str(_this.core_hmac_md5(A, B))
+        },
+        core_md5(K, F) {
+          var _this = this;
+          K[F >> 5] |= 128 << ((F) % 32);
+          K[(((F + 64) >>> 9) << 4) + 14] = F;
+          var J = 1732584193;
+          var I = -271733879;
+          var H = -1732584194;
+          var G = 271733878;
+          for (var C = 0; C < K.length; C += 16) {
+            var E = J;
+            var D = I;
+            var B = H;
+            var A = G;
+            J = _this.md5_ff(J, I, H, G, K[C + 0], 7, -680876936);
+            G = _this.md5_ff(G, J, I, H, K[C + 1], 12, -389564586);
+            H = _this.md5_ff(H, G, J, I, K[C + 2], 17, 606105819);
+            I = _this.md5_ff(I, H, G, J, K[C + 3], 22, -1044525330);
+            J = _this.md5_ff(J, I, H, G, K[C + 4], 7, -176418897);
+            G = _this.md5_ff(G, J, I, H, K[C + 5], 12, 1200080426);
+            H = _this.md5_ff(H, G, J, I, K[C + 6], 17, -1473231341);
+            I = _this.md5_ff(I, H, G, J, K[C + 7], 22, -45705983);
+            J = _this.md5_ff(J, I, H, G, K[C + 8], 7, 1770035416);
+            G = _this.md5_ff(G, J, I, H, K[C + 9], 12, -1958414417);
+            H = _this.md5_ff(H, G, J, I, K[C + 10], 17, -42063);
+            I = _this.md5_ff(I, H, G, J, K[C + 11], 22, -1990404162);
+            J = _this.md5_ff(J, I, H, G, K[C + 12], 7, 1804603682);
+            G = _this.md5_ff(G, J, I, H, K[C + 13], 12, -40341101);
+            H = _this.md5_ff(H, G, J, I, K[C + 14], 17, -1502002290);
+            I = _this.md5_ff(I, H, G, J, K[C + 15], 22, 1236535329);
+            J = _this.md5_gg(J, I, H, G, K[C + 1], 5, -165796510);
+            G = _this.md5_gg(G, J, I, H, K[C + 6], 9, -1069501632);
+            H = _this.md5_gg(H, G, J, I, K[C + 11], 14, 643717713);
+            I = _this.md5_gg(I, H, G, J, K[C + 0], 20, -373897302);
+            J = _this.md5_gg(J, I, H, G, K[C + 5], 5, -701558691);
+            G = _this.md5_gg(G, J, I, H, K[C + 10], 9, 38016083);
+            H = _this.md5_gg(H, G, J, I, K[C + 15], 14, -660478335);
+            I = _this.md5_gg(I, H, G, J, K[C + 4], 20, -405537848);
+            J = _this.md5_gg(J, I, H, G, K[C + 9], 5, 568446438);
+            G = _this.md5_gg(G, J, I, H, K[C + 14], 9, -1019803690);
+            H = _this.md5_gg(H, G, J, I, K[C + 3], 14, -187363961);
+            I = _this.md5_gg(I, H, G, J, K[C + 8], 20, 1163531501);
+            J = _this.md5_gg(J, I, H, G, K[C + 13], 5, -1444681467);
+            G = _this.md5_gg(G, J, I, H, K[C + 2], 9, -51403784);
+            H = _this.md5_gg(H, G, J, I, K[C + 7], 14, 1735328473);
+            I = _this.md5_gg(I, H, G, J, K[C + 12], 20, -1926607734);
+            J = _this.md5_hh(J, I, H, G, K[C + 5], 4, -378558);
+            G = _this.md5_hh(G, J, I, H, K[C + 8], 11, -2022574463);
+            H = _this.md5_hh(H, G, J, I, K[C + 11], 16, 1839030562);
+            I = _this.md5_hh(I, H, G, J, K[C + 14], 23, -35309556);
+            J = _this.md5_hh(J, I, H, G, K[C + 1], 4, -1530992060);
+            G = _this.md5_hh(G, J, I, H, K[C + 4], 11, 1272893353);
+            H = _this.md5_hh(H, G, J, I, K[C + 7], 16, -155497632);
+            I = _this.md5_hh(I, H, G, J, K[C + 10], 23, -1094730640);
+            J = _this.md5_hh(J, I, H, G, K[C + 13], 4, 681279174);
+            G = _this.md5_hh(G, J, I, H, K[C + 0], 11, -358537222);
+            H = _this.md5_hh(H, G, J, I, K[C + 3], 16, -722521979);
+            I = _this.md5_hh(I, H, G, J, K[C + 6], 23, 76029189);
+            J = _this.md5_hh(J, I, H, G, K[C + 9], 4, -640364487);
+            G = _this.md5_hh(G, J, I, H, K[C + 12], 11, -421815835);
+            H = _this.md5_hh(H, G, J, I, K[C + 15], 16, 530742520);
+            I = _this.md5_hh(I, H, G, J, K[C + 2], 23, -995338651);
+            J = _this.md5_ii(J, I, H, G, K[C + 0], 6, -198630844);
+            G = _this.md5_ii(G, J, I, H, K[C + 7], 10, 1126891415);
+            H = _this.md5_ii(H, G, J, I, K[C + 14], 15, -1416354905);
+            I = _this.md5_ii(I, H, G, J, K[C + 5], 21, -57434055);
+            J = _this.md5_ii(J, I, H, G, K[C + 12], 6, 1700485571);
+            G = _this.md5_ii(G, J, I, H, K[C + 3], 10, -1894986606);
+            H = _this.md5_ii(H, G, J, I, K[C + 10], 15, -1051523);
+            I = _this.md5_ii(I, H, G, J, K[C + 1], 21, -2054922799);
+            J = _this.md5_ii(J, I, H, G, K[C + 8], 6, 1873313359);
+            G = _this.md5_ii(G, J, I, H, K[C + 15], 10, -30611744);
+            H = _this.md5_ii(H, G, J, I, K[C + 6], 15, -1560198380);
+            I = _this.md5_ii(I, H, G, J, K[C + 13], 21, 1309151649);
+            J = _this.md5_ii(J, I, H, G, K[C + 4], 6, -145523070);
+            G = _this.md5_ii(G, J, I, H, K[C + 11], 10, -1120210379);
+            H = _this.md5_ii(H, G, J, I, K[C + 2], 15, 718787259);
+            I = _this.md5_ii(I, H, G, J, K[C + 9], 21, -343485551);
+            J = _this.safe_add(J, E);
+            I = _this.safe_add(I, D);
+            H = _this.safe_add(H, B);
+            G = _this.safe_add(G, A)
+          }
+          if (_this.mode == 16) {
+            return Array(I, H)
+          } else {
+            return Array(J, I, H, G)
+          }
+        },
+        md5_cmn(F, C, B, A, E, D) {
+          var _this = this;
+          return _this.safe_add(_this.bit_rol(_this.safe_add(_this.safe_add(C, F), _this.safe_add(A, D)), E), B)
+        },
+        md5_ff(C, B, G, F, A, E, D) {
+          var _this = this;
+          return _this.md5_cmn((B & G) | ((~B) & F), C, B, A, E, D)
+        },
+        md5_gg(C, B, G, F, A, E, D) {
+          var _this = this;
+          return _this.md5_cmn((B & F) | (G & (~F)), C, B, A, E, D)
+        },
+        md5_hh(C, B, G, F, A, E, D) {
+          var _this = this;
+          return _this.md5_cmn(B ^ G ^ F, C, B, A, E, D)
+        },
+        md5_ii(C, B, G, F, A, E, D) {
+          var _this = this;
+          return _this.md5_cmn(G ^ (B | (~F)), C, B, A, E, D)
+        },
+        core_hmac_md5(C, F) {
+          var _this = this;
+          var E = _this.str2binl(C);
+          if (E.length > 16) {
+            E = _this.core_md5(E, C.length * _this.chrsz)
+          }
+          var A = Array(16),
+            D = Array(16);
+          for (var B = 0; B < 16; B++) {
+            A[B] = E[B] ^ 909522486;
+            D[B] = E[B] ^ 1549556828
+          }
+          var G = _this.core_md5(A.concat(_this.str2binl(F)), 512 + F.length * _this.chrsz);
+          return _this.core_md5(D.concat(G), 512 + 128)
+        },
+        safe_add(A, D) {
+          var _this = this;
+          var C = (A & 65535) + (D & 65535);
+          var B = (A >> 16) + (D >> 16) + (C >> 16);
+          return (B << 16) | (C & 65535)
+        },
+        bit_rol(A, B) {
+          var _this = this;
+          return (A << B) | (A >>> (32 - B))
+        },
+        str2binl(D) {
+          var _this = this;
+          var C = Array();
+          var A = (1 << _this.chrsz) - 1;
+          for (var B = 0; B < D.length * _this.chrsz; B += _this.chrsz) {
+            C[B >> 5] |= (D.charCodeAt(B / _this.chrsz) & A) << (B % 32)
+          }
+          return C
+        },
+        binl2str(C) {
+          var _this = this;
+          var D = "";
+          var A = (1 << _this.chrsz) - 1;
+          for (var B = 0; B < C.length * 32; B += _this.chrsz) {
+            D += String.fromCharCode((C[B >> 5] >>> (B % 32)) & A)
+          }
+          return D
+        },
+        binl2hex(C) {
+          var _this = this;
+          var B = _this.hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+          var D = "";
+          for (var A = 0; A < C.length * 4; A++) {
+            D += B.charAt((C[A >> 2] >> ((A % 4) * 8 + 4)) & 15) + B.charAt((C[A >> 2] >> ((A % 4) * 8)) & 15)
+          }
+          return D
+        },
+        binl2b64 (D) {
+          var _this = this;
+          var C = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+          var F = "";
+          for (var B = 0; B < D.length * 4; B += 3) {
+            var E = (((D[B >> 2] >> 8 * (B % 4)) & 255) << 16) | (((D[B + 1 >> 2] >> 8 * ((B + 1) % 4)) & 255) << 8) | ((D[B + 2 >> 2] >> 8 * ((B + 2) % 4)) & 255);
+            for (var A = 0; A < 4; A++) {
+              if (B * 8 + A * 6 > D.length * 32) {
+                F += _this.b64pad
+              } else {
+                F += C.charAt((E >> 6 * (3 - A)) & 63)
+              }
+            }
+          }
+          return F
+        },
         // 从本地localStorage读取username
-        getLocalUsername () {
+        getLocalUsernameAndPass () {
           this.username = '' || localStorage.getItem('platformUserName');
+          this.password = '' || sessionStorage.getItem('platformPassword');
         },
         // 处理登录
         doLogin () {
+          var _this = this;
           var username = this.username;
           var password = this.password;
           var _this = this;
           if (username !== '' && password !== '') {
+            if (password.length < 32) {
+              _this.password = _this.md5(password);
+            }
             // 参数： 用户名、密码
             let postData = {
               username: username,
-              password: password
+              password: _this.password
             };
+            console.log(postData);
             axios.get('',postData).then(res => {
               var res = {
                 "code": 0,
@@ -64,6 +281,7 @@
                 if (res.data.length > 0) { // 验证通过
                   var data = res.data;
                   localStorage.setItem('platformUserName', username);
+                  sessionStorage.setItem('platformPassword', password);
                   console.log(localStorage.getItem('platformUserName'));
                   // 拿到userId,zoneId 保存在store里面
                   var userId = data[0].userId;
