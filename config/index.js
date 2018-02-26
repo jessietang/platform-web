bde66530afe9d316da7d111709ce9e914e638a60
jessietang <18380448313@163.com>
2018-02-26 15:14:27 +0800

    readme file

--- config/index.js
+++ config/index.js
@@ -26,7 +26,7 @@ module.exports = {
     port: 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static', // // 编译输出的二级目录
-    assetsPublicPath: '/', // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
+    assetsPublicPath: './', // 编译发布的根目录，可配置为资源服务器域名或 CDN 域名
     // 跨域设置(只适用于开发环境)
     proxyTable: {
       '/api': {
